69c5ab7ff3a9b62d17da2d1a5785290e6ec4fe0b
Michael Murray <michael.murray.iv@gmail.com>
2024-07-12 15:12:52 -0700

    updated to fix space

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,7 +1,8 @@
-def main():
-  string = input("Enter the string: ")
+def main(): # caesar cipher (lowercase only)
+  string = input("Enter the string: ").lower()
   for i in range(26):
-    print(f"Shift {i}: {''.join([chr((ord(c) - 65 + i) % 26 + 65) for c in string])}")
+    shifted_string = ''.join([chr((ord(c) - 97 + i) % 26 + 97) if 'a' <= c <= 'z' else c for c in string])
+    print(f"Shift {i}: {shifted_string}")
 
 if __name__ == "__main__":
   main()
